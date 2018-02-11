048fa03c526fdeeff77494edfcadc46c8fd635fb
Mark K Cowan <mark@battlesnake.co.uk>
2018-02-11 14:03:42 +0000

    config sync

--- start-qb.sh
+++ start-qb.sh
@@ -5,4 +5,4 @@ set -eu
 mkdir -p qb45/helper
 echo 'C:\QB45\QB.EXE /AH /H /L C:\QB45\QB.QLB' > qb45/helper/qb.bat
 
-dosbox -conf dosbox.conf -c 'KEYB UK' -c "mount c $PWD" -c 'C:' -c 'SET PATH=C:\QB45\HELPER;%PATH%'
+dosbox -fullscreen -conf dosbox.conf -c 'KEYB UK' -c "mount c $PWD" -c 'C:' -c 'SET PATH=C:\QB45\HELPER;%PATH%'
